8fab16f480ef923cd356e91c7fa0e407243429c4
Jeremias Santos <jeremiasmarinho05@gmail.com>
2026-01-26 15:27:53 -0300

    begins code structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
+import { MdArrowDropDown, MdBrightness2, MdBrightness5 } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={styles.headerTextGroup}>
+        <p>jeremias marinho | 2026</p>
+        <p>hope you enjoy it</p>
+      </div>
+      <h1 className={styles.title}>portfólio.</h1>
+
+      <div className={styles.tagGroup}>
+        <p className={styles.tag}>UI design</p>
+        <p className={styles.tag}>design gráfico</p>
+        <p className={styles.tag}>social media</p>
+        <p className={styles.tag}>identidades visuais</p>
+      </div>
+
+      <div className={styles.homeButtonsGroup}>
+        <div className={styles.homeButton}>
+          idioma <MdArrowDropDown />
+        </div>
+        <div className={styles.homeButton}>
+          <MdBrightness5 />
+        </div>
+      </div>
+    </>
   );
 }
 
