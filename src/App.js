719f68fc1b693e5837b67a48ca6b8115846aaead
Jeremias Santos <jeremiasmarinho05@gmail.com>
2026-01-31 10:33:08 -0300

    add project component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import styles from "./App.module.css";
 
 import Home from "./pages/home/Home";
+import Project from "./pages/project/Project";
+
 function App() {
-  return <Home />;
+  return (
+    <>
+      {/* <Home />; */}
+      <Project />
+    </>
+  );
 }
 
 export default App;
